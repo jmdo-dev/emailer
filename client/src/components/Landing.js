887629b7ecbf179735f15b4633f048901106a8ad
Joe OMalley <joemdomalley@gmail.com>
2020-02-26 11:11:36 +0000

    first deploy

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Landing = () => {
 	return (
 		<div style={{ textAlign: 'center' }}>
-			<h1>Emaily</h1>
-			Collect feedback from your users
+			<h1>eMAILer</h1>
+			<h5>Collect feedback from your users</h5>
 		</div>
 	);
 };
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
 		keys: [keys.cookieKey],
-	}),
+	})
 );
 
 app.use(passport.initialize());
@@ -38,6 +38,7 @@ if (process.env.NODE_ENV === 'production') {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
